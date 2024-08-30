0d7194f5a85ad3e9be8dc4e7dca8d404a0183419
Gursheen Anand <gursheen@frappe.io>
2024-08-30 14:23:11 +0530

    feat: add basic navbar and canvas

--- frontend/src/apps/slides/App.vue
+++ frontend/src/apps/slides/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="text-base">
 		<router-view />
 	</div>
 </template>
--- frontend/src/apps/slides/pages/Home.vue
+++ frontend/src/apps/slides/pages/Home.vue
@@ -1,5 +1,28 @@
 <template>
-	<div></div>
+	<div class="fixed flex h-screen w-screen flex-col bg-gray-50">
+		<div class="flex justify-between bg-white p-2 shadow-xl shadow-gray-200 items-center">
+			<div class="flex items-center gap-2">
+				<Logo />
+				<div class="font-semibold">Slides</div>
+			</div>
+
+			<div class="flex justify-between">
+				<Button variant="solid" label="Present" size="sm">
+					<template #prefix>
+						<SlideshowIcon class="h-4 w-4" />
+					</template>
+				</Button>
+			</div>
+		</div>
+
+		<div class="flex h-full items-center justify-center">
+			<!-- slide dimensions: 16:9 ratio -->
+			<div class="h-[540px] w-[960px] bg-white drop-shadow-lg"></div>
+		</div>
+	</div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import Logo from '@/icons/Logo.vue'
+import SlideshowIcon from '@/icons/SlideshowIcon.vue'
+</script>
