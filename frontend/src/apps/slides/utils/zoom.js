fdec90d3cc34165a54431d718c550200d8b73939
Gursheen Anand <gursheen@frappe.io>
2024-09-03 10:27:45 +0530

    refactor: cleanup exported util

--- frontend/src/apps/slides/utils/zoom.js
+++ frontend/src/apps/slides/utils/zoom.js
@@ -5,43 +5,46 @@ const SCALE_SPEED = 0.5
 const TRANSLATE_SPEED = 0.5
 
 let target, origin, transform
-let initialTransform = new DOMMatrix()
 
-const startPanOrZoom = () => {
+let initialMatrix = new DOMMatrix()
+let gestureMatrix = new DOMMatrix()
+
+let wheelTimeout = null
+
+const setOrigin = () => {
+	// origin is the midpoint of the initial touch points
 	let targetRect = target.getBoundingClientRect()
 	origin = {
 		x: transform.origin.x - targetRect.x,
 		y: transform.origin.y - targetRect.y,
 	}
-	let m = getMatrix(transform).multiply(initialTransform)
-	target.style.transform = m
 }
 
-const updateTransform = () => {
+const startGesture = () => {
+	setOrigin()
+	setGestureMatrix()
+	applyMatrix()
+}
+
+const updateGesture = () => {
 	limitScale()
 	limitTranslation()
-	let m = getMatrix(transform).multiply(initialTransform)
-	target.style.transform = m
+	setGestureMatrix()
+	applyMatrix()
 }
 
-const endPanOrZoom = () => {
+const endGesture = () => {
 	limitScale()
 	limitTranslation()
-	initialTransform = getMatrix(transform).multiply(initialTransform)
-	target.style.transform = initialTransform
-}
-
-const getMatrix = () => {
-	return new DOMMatrix()
-		.translate(origin.x, origin.y)
-		.translate(transform.translation.x, transform.translation.y)
-		.scale(transform.scale || 1)
-		.translate(-origin.x, -origin.y)
+	setGestureMatrix()
+	// while ending transform, apply the final transformation matrix
+	initialMatrix = gestureMatrix
+	applyMatrix()
 }
 
 const limitScale = () => {
 	// previous scale for transformation matrix is length of hypotenuse of x and y components
-	let scale = Math.hypot(initialTransform.a, initialTransform.b)
+	let scale = Math.hypot(initialMatrix.a, initialMatrix.b)
 	let currentScale = transform.scale * scale
 
 	// limit scale between 0.5 - 2
@@ -51,69 +54,93 @@ const limitScale = () => {
 }
 
 const limitTranslation = () => {
-	let nextX = transform.translation.x + initialTransform.e
-	let nextY = transform.translation.y + initialTransform.f
+	let nextX = transform.translation.x + initialMatrix.e
+	let nextY = transform.translation.y + initialMatrix.f
 
-	let scale = Math.hypot(initialTransform.a, initialTransform.b)
+	let scale = Math.hypot(initialMatrix.a, initialMatrix.b)
 
 	// if target goes out of bounds from top or bottom
 	// reduce the translation on Y axis
 	let ylimit = scale < 1 ? 600 * scale : 600
-	if (nextY > ylimit) transform.translation.y = ylimit - initialTransform.f
-	else if (nextY < -ylimit) transform.translation.y = -ylimit - initialTransform.f
+	if (nextY > ylimit) transform.translation.y = ylimit - initialMatrix.f
+	else if (nextY < -ylimit) transform.translation.y = -ylimit - initialMatrix.f
 
 	// if target goes out of bounds from top or bottom
 	// reduce the translation on X axis
 	let xlimit = scale < 1 ? 1150 * scale : 1150
-	if (nextX > xlimit) transform.translation.x = xlimit - initialTransform.e
-	else if (nextX < -xlimit) transform.translation.x = -xlimit - initialTransform.e
+	if (nextX > xlimit) transform.translation.x = xlimit - initialMatrix.e
+	else if (nextX < -xlimit) transform.translation.x = -xlimit - initialMatrix.e
+}
+
+const setGestureMatrix = () => {
+	let matrix = new DOMMatrix()
+		.translate(origin.x, origin.y)
+		.translate(transform.translation.x, transform.translation.y)
+		.scale(transform.scale || 1)
+		.translate(-origin.x, -origin.y)
+
+	gestureMatrix = matrix.multiply(initialMatrix)
+}
+
+const applyMatrix = () => {
+	target.style.transform = gestureMatrix.toString()
+}
+
+const setZoomTransform = (e) => {
+	let dy = e.deltaY * SCALE_SPEED
+	let factor = e.deltaY <= 0 ? 1 - dy / 100 : 1 / (1 + dy / 100)
+	transform = {
+		origin: { x: e.clientX, y: e.clientY },
+		scale: transform.scale * factor,
+		translation: transform.translation,
+	}
+}
+
+const setPanTransform = (e) => {
+	let dx = e.deltaX * TRANSLATE_SPEED
+	let dy = e.deltaY * TRANSLATE_SPEED
+	transform = {
+		origin: { x: e.clientX, y: e.clientY },
+		scale: transform.scale,
+		translation: {
+			x: transform.translation.x - dx,
+			y: transform.translation.y - dy,
+		},
+	}
+}
+
+const handlePanAndZoom = (e) => {
+	e.preventDefault()
+
+	// initialize the transform object
+	if (!transform) {
+		transform = {
+			origin: { x: e.clientX, y: e.clientY },
+			scale: 1,
+			translation: { x: 0, y: 0 },
+		}
+		startGesture()
+	}
+
+	if (e.ctrlKey) {
+		setZoomTransform(e)
+	} else {
+		setPanTransform(e)
+	}
+
+	// incrementally update the transform object
+	updateGesture(e)
+
+	if (wheelTimeout) window.clearTimeout(wheelTimeout)
+	wheelTimeout = setTimeout(() => {
+		// apply the final transformation
+		endGesture()
+		transform = null
+	}, 200)
 }
 
 const addPanAndZoom = (containerElement, targetElement) => {
 	target = targetElement
-	let wheelTimeout = null
-
-	const handlePanAndZoom = (e) => {
-		e.preventDefault()
-
-		if (!transform) {
-			transform = {
-				origin: { x: e.clientX, y: e.clientY },
-				scale: 1,
-				translation: { x: 0, y: 0 },
-			}
-			startPanOrZoom()
-		}
-
-		if (e.ctrlKey) {
-			let zoom_factor = e.deltaY <= 0 ? 1 - e.deltaY / 100 : 1 / (1 + e.deltaY / 100)
-			transform = {
-				origin: { x: e.clientX, y: e.clientY },
-				scale: transform.scale * zoom_factor,
-				translation: transform.translation,
-			}
-		} else {
-			let dx = e.deltaX * TRANSLATE_SPEED
-			let dy = e.deltaY * TRANSLATE_SPEED
-			transform = {
-				origin: { x: e.clientX, y: e.clientY },
-				scale: transform.scale,
-				translation: {
-					x: transform.translation.x - dx,
-					y: transform.translation.y - dy,
-				},
-			}
-		}
-
-		updateTransform()
-
-		if (wheelTimeout) window.clearTimeout(wheelTimeout)
-		wheelTimeout = setTimeout(() => {
-			endPanOrZoom()
-			transform = null
-		}, 200)
-	}
-
 	containerElement.addEventListener('wheel', handlePanAndZoom, {
 		passive: false,
 	})
