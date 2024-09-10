4100e514960f7c87b3f5de31a089c8555c4390f8
Gursheen Anand <gursheen@frappe.io>
2024-09-10 16:20:33 +0530

    feat: layout for font properties

--- frontend/src/apps/slides/components/SlideElementsPanel.vue
+++ frontend/src/apps/slides/components/SlideElementsPanel.vue
@@ -60,7 +60,7 @@
 
 	<!-- Element Properties Panel -->
 	<div
-		class="fixed z-20 flex h-[743px] w-[226px] flex-col bg-white shadow-xl shadow-gray-200 transition-all duration-500 ease-in-out"
+		class="z-5 fixed flex h-[743px] w-[226px] flex-col bg-white shadow-xl shadow-gray-200 transition-all duration-500 ease-in-out"
 		:class="activeElement ? 'right-13' : '-right-[174px]'"
 	>
 		<div v-if="activeTab == 'slide'">
@@ -75,7 +75,7 @@
 		</div>
 
 		<div v-else-if="activeTab == 'text'">
-			<div class="flex flex-col gap-4 border-b px-4 py-2">
+			<div class="flex flex-col gap-4 px-4 py-2">
 				<div class="flex items-center justify-between">
 					<div class="cursor-pointer rounded-sm p-1">
 						<FeatherIcon name="bold" class="h-4" />
@@ -110,6 +110,29 @@
 					<div class="cursor-pointer rounded-sm p-1">
 						<FeatherIcon name="list" class="h-4.5" />
 					</div>
+				</div>
+			</div>
+
+			<div class="flex flex-col gap-4 border-y px-4 py-4">
+				<div class="text-2xs uppercase text-gray-600">Font</div>
+				<FormControl type="autocomplete" :options="textFonts" size="sm" variant="subtle" />
+
+				<div class="flex items-center justify-between">
+					<div class="flex h-7 w-3/5 rounded border bg-gray-100">
+						<div class="flex w-10 cursor-pointer items-center justify-center">
+							<FeatherIcon name="minus" class="h-3" strokeWidth="2" />
+						</div>
+						<div class="bg-white">
+							<input
+								type="number"
+								class="h-full w-12 border-none p-0 text-center text-xs font-semibold text-gray-800 focus:outline-none focus:ring-0"
+							/>
+						</div>
+						<div class="flex w-10 cursor-pointer items-center justify-center rounded-r">
+							<FeatherIcon name="plus" class="h-3" strokeWidth="2" />
+						</div>
+					</div>
+					<div class="h-6 w-6 cursor-pointer rounded border bg-black shadow-sm"></div>
 				</div>
 			</div>
 		</div>
@@ -148,4 +171,27 @@ const addTextElement = () => {
 
 	document.querySelector('.slide').appendChild(text)
 }
+
+const textFonts = [
+	{
+		label: 'Arial',
+		value: 'Arial',
+	},
+	{
+		label: 'Roboto',
+		value: 'Roboto',
+	},
+	{
+		label: 'Helvetica',
+		value: 'Helvetica',
+	},
+]
 </script>
+
+<style scoped>
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+	-webkit-appearance: none;
+	margin: 0;
+}
+</style>
