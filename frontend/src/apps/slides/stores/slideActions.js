9381205f3950e553d94e31384fd96fbe7cd19199
Gursheen Anand <gursheen@frappe.io>
2024-12-17 16:23:55 +0530

    refactor: simplify updating and loading slide state

--- frontend/src/apps/slides/stores/slideActions.js
+++ frontend/src/apps/slides/stores/slideActions.js
@@ -14,54 +14,50 @@ import {
 } from './slide'
 import { resetFocus } from './element'
 
-const updateSlideThumbnail = (slideDiv, index) => {
-	if (!slideDiv) return
-	html2canvas(slideDiv).then((canvas) => {
+const updateSlideThumbnail = (index) => {
+	if (inSlideShow.value) return
+	const slideRef = document.querySelector('.slide')
+	html2canvas(slideRef).then((canvas) => {
 		let img = canvas.toDataURL('image/png')
 		presentation.data.slides[index].thumbnail = img
 	})
 }
 
-const changeSlide = (index) => {
-	if (index < 0 || index >= presentation.data.slides.length) return
-	presentation.data.slides[activeSlideIndex.value].elements = JSON.stringify(
-		activeSlideElements.value,
-	)
-	presentation.data.slides[activeSlideIndex.value].transition = slideTransition.value
-	presentation.data.slides[activeSlideIndex.value].transition_duration =
-		slideTransitionDuration.value
-	resetFocus()
+const updateSlideState = () => {
+	let currentSlide = presentation.data.slides[activeSlideIndex.value]
+	currentSlide.elements = JSON.stringify(activeSlideElements.value, null, 2)
+	currentSlide.transition = slideTransition.value
+	currentSlide.transition_duration = slideTransitionDuration.value
+}
+
+const triggerTransition = (index) => {
 	currentTransitionSlide.value = index
 	applyReverseTransition.value = index < activeSlideIndex.value
+}
+
+const loadSlide = (index) => {
+	const { transition, transition_duration, elements } = presentation.data.slides[index]
+
+	slideTransition.value = transition
+	slideTransitionDuration.value = transition_duration
+	activeSlideElements.value = elements ? JSON.parse(elements) : []
+}
+
+const changeSlide = (index) => {
+	if (index < 0 || index >= presentation.data.slides.length) return
+	resetFocus()
+	updateSlideState()
+	triggerTransition(index)
 	nextTick(() => {
-		if (!inSlideShow.value) {
-			let slideRef = document.querySelector('.slide')
-			if (slideRef) {
-				updateSlideThumbnail(slideRef, activeSlideIndex.value)
-			}
-		}
+		updateSlideThumbnail(activeSlideIndex.value)
 		activeSlideIndex.value = index
-		slideTransition.value = presentation.data.slides[activeSlideIndex.value].transition
-		slideTransitionDuration.value =
-			presentation.data.slides[activeSlideIndex.value].transition_duration
-		if (presentation.data.slides[activeSlideIndex.value].elements)
-			activeSlideElements.value = JSON.parse(
-				presentation.data.slides[activeSlideIndex.value].elements,
-			)
-		else activeSlideElements.value = []
+		loadSlide(activeSlideIndex.value)
 	})
 }
 
 const saveChanges = async () => {
 	if (!presentation.data) return
-	presentation.data.slides[activeSlideIndex.value].elements = JSON.stringify(
-		activeSlideElements.value,
-		null,
-		2,
-	)
-	presentation.data.slides[activeSlideIndex.value].transition = slideTransition.value
-	presentation.data.slides[activeSlideIndex.value].transition_duration =
-		slideTransitionDuration.value
+	updateSlideState()
 	await call('frappe.client.save', {
 		doc: presentation.data,
 	})
