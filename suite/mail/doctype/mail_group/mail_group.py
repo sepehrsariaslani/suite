26d464b2203b1e26519d4a0837e4f6170abdb795
s-aga-r <sagarsharma.s312@gmail.com>
2025-01-07 09:18:47 +0530

    fix: remove group from agents when disabled

--- suite/mail/doctype/mail_group/mail_group.py
+++ suite/mail/doctype/mail_group/mail_group.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 from mail.mail.doctype.mail_agent_job.mail_agent_job import (
@@ -22,21 +23,35 @@ class MailGroup(Document):
 		self.name = self.email
 
 	def validate(self) -> None:
+		self.validate_enabled()
 		self.validate_domain()
 		self.validate_email()
-		self.validate_display_name()
 
 	def on_update(self) -> None:
-		if self.has_value_changed("email"):
-			create_group_on_agents(self.email, self.display_name)
-			return
-
-		has_value_changed = self.has_value_changed("display_name")
-		if has_value_changed:
-			patch_group_on_agents(self.email, self.display_name)
+		if self.enabled:
+			if self.has_value_changed("enabled") or self.has_value_changed("email"):
+				create_group_on_agents(self.email, self.display_name)
+			elif self.has_value_changed("display_name"):
+				patch_group_on_agents(self.email, self.display_name)
+		elif self.has_value_changed("enabled"):
+			delete_group_from_agents(self.email)
 
 	def on_trash(self) -> None:
 		delete_group_from_agents(self.email)
+
+	def validate_enabled(self) -> None:
+		"""Validates the enabled field."""
+
+		if self.enabled:
+			return
+
+		if alias := frappe.db.exists(
+			"Mail Alias", {"enabled": 1, "alias_for_type": self.doctype, "alias_for_name": self.name}
+		):
+			frappe.throw(_("Mail Alias {0} is enabled. Please disable it first.").format(frappe.bold(alias)))
+
+		if frappe.db.exists("Mail Group Member", {"mail_group": self.name}):
+			frappe.throw(_("Group has members. Please remove them first."))
 
 	def validate_domain(self) -> None:
 		"""Validates the domain."""
@@ -48,9 +63,3 @@ class MailGroup(Document):
 
 		is_email_assigned(self.email, self.doctype, raise_exception=True)
 		is_valid_email_for_domain(self.email, self.domain_name, raise_exception=True)
-
-	def validate_display_name(self) -> None:
-		"""Validates the display name."""
-
-		if self.is_new() and not self.display_name:
-			self.display_name = frappe.db.get_value("User", self.user, "full_name")
