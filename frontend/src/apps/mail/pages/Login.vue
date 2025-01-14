f0815fea6a5823bbfdc444f6a10fba44a83ede6c
krantheman <akash.akasht.tom@gmail.com>
2025-01-14 14:26:54 +0530

    feat: add initial Sign Up and Login dialog

--- frontend/src/apps/mail/pages/Login.vue
+++ frontend/src/apps/mail/pages/Login.vue
@@ -1,4 +1,37 @@
 <template>
-	<div>Hello World</div>
+	<div>
+		<div class="mb-6 text-center">
+			<span class="text-center text-lg font-medium leading-5 tracking-tight text-gray-900">
+				Sign in to your account
+			</span>
+		</div>
+		<form class="flex flex-col space-y-4">
+			<FormControl
+				label="Email"
+				type="email"
+				placeholder="johndoe@mail.com"
+				autocomplete="email"
+				v-model="email"
+				required
+			/>
+			<FormControl
+				label="Password"
+				type="password"
+				placeholder="••••••••"
+				v-model="password"
+				name="password"
+				autocomplete="current-password"
+				required
+			/>
+			<Button variant="solid"> Log In </Button>
+		</form>
+		<div class="mt-6 text-center hover:underline">
+			<router-link class="text-center text-base font-medium" to="signup">
+				New member? Create an account.
+			</router-link>
+		</div>
+	</div>
 </template>
-<script setup></script>
+<script setup>
+import { FormControl, Button } from 'frappe-ui'
+</script>
--- frontend/src/apps/mail/pages/SignUp.vue
+++ frontend/src/apps/mail/pages/SignUp.vue
@@ -1,3 +1,28 @@
 <template>
-	<div>Hello World</div>
+	<div>
+		<div class="mb-6 text-center">
+			<span class="text-center text-lg font-medium leading-5 tracking-tight text-gray-900">
+				Create a new account
+			</span>
+		</div>
+		<form class="flex flex-col space-y-4">
+			<FormControl
+				label="Email"
+				type="email"
+				placeholder="johndoe@mail.com"
+				autocomplete="email"
+				v-model="email"
+				required
+			/>
+			<Button variant="solid"> Sign Up </Button>
+		</form>
+		<div class="mt-6 text-center hover:underline">
+			<router-link class="text-center text-base font-medium" to="login">
+				Already have an account? Log in.
+			</router-link>
+		</div>
+	</div>
 </template>
+<script setup>
+import { FormControl, Button } from 'frappe-ui'
+</script>
