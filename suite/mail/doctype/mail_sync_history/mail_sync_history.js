bb6f2ba4f51cfa0d24f53d1204324dde891fce5e
s-aga-r <sagarsharma.s312@gmail.com>
2025-01-16 12:29:37 +0530

    chore: add link filters in mail sync history

--- suite/mail/doctype/mail_sync_history/mail_sync_history.js
+++ suite/mail/doctype/mail_sync_history/mail_sync_history.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Mail Sync History", {
-// 	refresh(frm) {
+frappe.ui.form.on("Mail Sync History", {
+	setup(frm) {
+		frm.trigger("set_queries");
+	},
 
-// 	},
-// });
+	set_queries(frm) {
+		frm.set_query("user", () => ({
+			filters: {
+				enabled: 1,
+			},
+		}));
+
+		frm.set_query("account", () => ({
+			filters: {
+				enabled: 1,
+			},
+		}));
+	},
+});
