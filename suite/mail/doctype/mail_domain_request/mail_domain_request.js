37a33fe942cf8010fb6a652cf2fb3a826a5d2e5a
s-aga-r <sagarsharma.s312@gmail.com>
2025-01-27 19:41:35 +0530

    refactor: Mail Domain Request

--- suite/mail/doctype/mail_domain_request/mail_domain_request.js
+++ suite/mail/doctype/mail_domain_request/mail_domain_request.js
@@ -2,8 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Mail Domain Request", {
-	refresh: function (frm) {
+	refresh(frm) {
+		frm.trigger("add_actions");
+	},
+
+	add_actions(frm) {
 		if (frm.doc.__islocal) return;
+
 		frm.add_custom_button(__("Verify DNS Record"), () => {
 			frm.call("verify_key");
 		});
--- suite/mail/doctype/mail_domain_request/mail_domain_request.py
+++ suite/mail/doctype/mail_domain_request/mail_domain_request.py
@@ -12,22 +12,37 @@ from mail.utils.dns import verify_dns_record
 
 
 class MailDomainRequest(Document):
-	def validate(self):
+	def validate(self) -> None:
+		self.validate_domain_name()
+
+	def before_insert(self) -> None:
+		self.set_verification_key()
+
+	def validate_domain_name(self) -> None:
+		"""Validates the domain name"""
+
 		domain_regex = r"^(?!:\/\/)([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,63}$"
 		if re.fullmatch(domain_regex, self.domain_name) is None:
 			frappe.throw(_("Invalid domain name"))
 
-	def before_insert(self):
+	def set_verification_key(self) -> None:
+		"""Set the verification key"""
+
 		self.verification_key = "frappe-mail-verification=" + random_string(48)
 
 	@frappe.whitelist()
-	def verify_key(self):
+	def verify_key(self) -> bool:
+		"""Verify the DNS record"""
+
 		self.is_verified = verify_dns_record(self.domain_name, "TXT", self.verification_key)
 		if self.is_verified:
 			self.create_domain()
+
 		return self.is_verified
 
-	def create_domain(self):
+	def create_domain(self) -> str:
+		"""Create the mail domain"""
+
 		domain = frappe.new_doc("Mail Domain")
 		domain.domain_name = self.domain_name
 		domain.mail_tenant = self.mail_tenant
