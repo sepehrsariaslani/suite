afb9ebb29ec1ba68a227c61cb79dc29ab43eae54
s-aga-r <sagarsharma.s312@gmail.com>
2025-02-04 11:22:48 +0530

    chore: remove composite unique index from Mail Recipient

--- suite/mail/doctype/mail_recipient/mail_recipient.py
+++ suite/mail/doctype/mail_recipient/mail_recipient.py
@@ -9,11 +9,3 @@ from uuid_utils import uuid7
 class MailRecipient(Document):
 	def autoname(self) -> None:
 		self.name = str(uuid7())
-
-
-def on_doctype_update() -> None:
-	frappe.db.add_unique(
-		"Mail Recipient",
-		["parent", "type", "email"],
-		constraint_name="unique_parent_type_email",
-	)
