c43d01f0208f336183e595b416809925791ab14c
s-aga-r <sagarsharma.s312@gmail.com>
2025-02-10 14:45:41 +0530

    perf: get rate limits from cache

--- suite/mail/utils/rate_limiter.py
+++ suite/mail/utils/rate_limiter.py
@@ -3,36 +3,28 @@ from functools import wraps
 import frappe
 from frappe.rate_limiter import rate_limit
 
+from mail.utils.cache import get_rate_limits
+
 
 def dynamic_rate_limit() -> callable:
+	"""A decorator to apply rate limits dynamically based on the method path."""
+
 	def decorator(fn):
 		@wraps(fn)
 		def wrapper(*args, **kwargs):
 			method_path = frappe.form_dict.cmd
-			rate_limits = get_rate_limit(method_path)
+			rate_limits = get_rate_limits(method_path)
 
 			wrapped_fn = fn
 			for rl in rate_limits:
 				if rl["ignore_in_developer_mode"] and frappe.conf.developer_mode:
 					continue
 
-				key = rl["key"]
-				limit = rl["limit"]
-				seconds = rl["seconds"]
-				methods = rl["methods"].split(",")
-				ip_based = bool(rl["ip_based"])
-				wrapped_fn = rate_limit(key, limit, seconds, methods, ip_based)(wrapped_fn)
+				rl.pop("ignore_in_developer_mode")
+				wrapped_fn = rate_limit(**rl)(wrapped_fn)
 
 			return wrapped_fn(*args, **kwargs)
 
 		return wrapper
 
 	return decorator
-
-
-def get_rate_limit(method_path: str) -> list:
-	return frappe.db.get_all(
-		"Rate Limit",
-		filters={"enabled": 1, "method_path": method_path},
-		fields=["ignore_in_developer_mode", "key", "limit", "seconds", "methods", "ip_based"],
-	)
