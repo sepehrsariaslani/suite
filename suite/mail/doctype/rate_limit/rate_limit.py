4ebbb8b4d0ce5b2ff17de1d71ebdf0cb5659e66a
s-aga-r <sagarsharma.s312@gmail.com>
2025-02-10 16:38:47 +0530

    fix: clear cache on rate limit update or delete

--- suite/mail/doctype/rate_limit/rate_limit.py
+++ suite/mail/doctype/rate_limit/rate_limit.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class RateLimit(Document):
-	pass
+	def on_update(self) -> None:
+		self.clear_cache()
+
+	def on_trash(self) -> None:
+		self.clear_cache()
+
+	def clear_cache(self) -> None:
+		"""Clear cache for the rate limit"""
+
+		frappe.cache.hdel("rate_limits", self.method_path)
