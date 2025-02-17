ed988f31b3c193e3edc21aa130a8022a4f711039
s-aga-r <sagarsharma.s312@gmail.com>
2025-02-17 15:16:08 +0530

    refactor: `mail.utils.enqueue_job`

--- suite/mail/tasks.py
+++ suite/mail/tasks.py
@@ -11,7 +11,7 @@ def enqueue_transfer_emails_to_agent() -> None:
 	"Called by the scheduler to enqueue the `transfer_emails_to_agent` job."
 
 	frappe.session.user = "Administrator"
-	enqueue_job(transfer_emails_to_agent, queue="long")
+	enqueue_job(transfer_emails_to_agent, queue="long", deduplicate=True)
 
 
 @frappe.whitelist()
@@ -19,7 +19,7 @@ def enqueue_delete_newsletters() -> None:
 	"Called by the scheduler to enqueue the `delete_newsletters` job."
 
 	frappe.session.user = "Administrator"
-	enqueue_job(delete_newsletters, queue="long")
+	enqueue_job(delete_newsletters, queue="long", deduplicate=True)
 
 
 @frappe.whitelist()
@@ -27,11 +27,14 @@ def enqueue_fetch_emails_from_mail_agents() -> None:
 	"Called by the scheduler to enqueue the `fetch_emails_from_mail_agents` job."
 
 	frappe.session.user = "Administrator"
-	enqueue_job(fetch_emails_from_mail_agents, queue="long")
+	enqueue_job(fetch_emails_from_mail_agents, queue="long", deduplicate=True)
 
 
 @frappe.whitelist()
 def enqueue_verify_all_dns_records() -> None:
 	"Called by the scheduler to enqueue the `verify_all_dns_records` job."
 
-	enqueue_job(verify_all_dns_records, queue="long")
+	frappe.only_for("System Manager")
+
+	frappe.session.user = "Administrator"
+	enqueue_job(verify_all_dns_records, queue="long", deduplicate=True)
