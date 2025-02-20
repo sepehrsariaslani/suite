249fc02edc8788528344fc7ccac1d7dd64cbde1a
krantheman <akash.akasht.tom@gmail.com>
2025-02-20 17:06:57 +0530

    refactor(SidebarDetail): show status down and add empty body state

--- frontend/src/apps/mail/components/SidebarDetail.vue
+++ frontend/src/apps/mail/components/SidebarDetail.vue
@@ -1,30 +1,38 @@
 <template>
 	<div class="space-y-1 p-2.5">
 		<div class="flex items-center justify-between">
-			<div class="mr-2 flex items-center space-x-2 truncate">
-				<h2 class="truncate font-semibold">{{ mail.subject || __('(No Subject)') }}</h2>
-				<Badge
-					v-if="Object.keys(STATUS).includes(badgeField)"
-					:label="badge.label"
-					:theme="badge.theme"
-					size="sm"
-					class="!text-[11px]"
-				/>
-			</div>
+			<h2 class="mr-1 truncate font-semibold" :class="{ italic: !mail.subject }">
+				{{ mail.subject || __('[No subject]') }}
+			</h2>
 			<MailDate :datetime="mail.creation" :in-list="true" />
 		</div>
-
 		<h3 class="snippet line-clamp-1 text-xs">
 			{{ mail.display_name ? mail.display_name : mail.sender }}
 		</h3>
-		<h4 class="snippet line-clamp-2 h-9 text-xs text-gray-600">
-			{{ mail.snippet }}
+		<h4
+			class="snippet line-clamp-2 h-9 text-xs text-gray-600"
+			:class="{ italic: !mail.snippet }"
+		>
+			{{ mail.snippet || __('— No message body —') }}
 		</h4>
+		<div class="flex items-center">
+			<!-- <div class="flex rounded bg-gray-100 p-1 text-gray-700">
+				<Paperclip class="mr-1 h-3.5 w-3.5" />
+				<span class="text-xs">{{ 20 }}</span>
+			</div> -->
+			<Badge
+				class="ml-auto"
+				v-if="Object.keys(STATUS).includes(badgeField)"
+				:label="badge.label"
+				:theme="badge.theme"
+			/>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
+import { Paperclip } from 'lucide-vue-next'
 import { Badge } from 'frappe-ui'
 
 import MailDate from '@/components/MailDate.vue'
