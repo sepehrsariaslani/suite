f3c8ff3d25451e5cfffce61ea0736315d5c3d48f
Gursheen Anand <gursheen@frappe.io>
2025-03-30 00:09:01 +0530

    refactor: simplify common component

--- frontend/src/apps/slides/components/PropertiesPanel.vue
+++ frontend/src/apps/slides/components/PropertiesPanel.vue
@@ -3,56 +3,47 @@
 		class="fixed right-0 z-20 flex flex-col h-[94.35%] w-64 bg-white border-l shadow-[0_10px_24px_-3px_rgba(199,199,199,0.6)]"
 		@wheel.prevent
 	>
-		<SlideProperties v-if="activeTab == 'slide'" />
+		<component :is="activeProperties" />
 
-		<TextProperties v-else-if="activeTab == 'text'" />
-
-		<ImageProperties v-else-if="activeTab == 'image'" />
-
-		<VideoProperties v-else-if="activeTab == 'video'" />
-
-		<div v-else>
-			<div v-if="activeElement && activeTab != 'video'" :class="sectionClasses">
-				<div :class="sectionTitleClasses">Other</div>
-				<SliderInput
-					label="Opacity"
-					:rangeStart="0"
-					:rangeEnd="100"
-					:modelValue="parseFloat(activeElement.opacity) || 100"
-					@update:modelValue="(value) => (activeElement.opacity = value)"
-				/>
-			</div>
+		<div v-if="activeElement" :class="sectionClasses">
+			<div :class="sectionTitleClasses">Other</div>
+			<SliderInput
+				label="Opacity"
+				:rangeStart="0"
+				:rangeEnd="100"
+				:modelValue="parseFloat(activeElement.opacity) || 100"
+				@update:modelValue="(value) => (activeElement.opacity = value)"
+			/>
 		</div>
 	</div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { computed } from 'vue'
 
 import { FormControl } from 'frappe-ui'
-import { FlipHorizontal, FlipVertical, Repeat2, TvMinimalPlay, Ban } from 'lucide-vue-next'
 
-import SlideProperties from './SlideProperties.vue'
+import SlideProperties from '@/components/SlideProperties.vue'
 import TextProperties from '@/components/TextProperties.vue'
-import ImageProperties from './ImageProperties.vue'
-import VideoProperties from './VideoProperties.vue'
+import ImageProperties from '@/components/ImageProperties.vue'
+import VideoProperties from '@/components/VideoProperties.vue'
 
 import SliderInput from '@/components/controls/SliderInput.vue'
-import NumberInput from '@/components/controls/NumberInput.vue'
-import ColorPicker from '@/components/controls/ColorPicker.vue'
-import CollapsibleSection from './controls/CollapsibleSection.vue'
 
 import { presentation } from '@/stores/presentation'
-import { slide, slideIndex } from '@/stores/slide'
+import { slide } from '@/stores/slide'
 import { activeElement } from '@/stores/element'
+import { sectionClasses, sectionTitleClasses } from '@/utils/constants'
 
-const activeTab = computed(() => {
-	if (activeElement.value) return activeElement.value.type
-	return 'slide'
+const activeProperties = computed(() => {
+	const elementType = activeElement.value?.type
+
+	if (!elementType) return SlideProperties
+
+	if (elementType == 'text') return TextProperties
+	if (elementType == 'image') return ImageProperties
+	if (elementType == 'video') return VideoProperties
 })
-
-const sectionClasses = 'flex flex-col gap-4 p-4 border-b'
-const sectionTitleClasses = 'text-2xs font-semibold uppercase text-gray-700'
 </script>
 
 <style scoped>
