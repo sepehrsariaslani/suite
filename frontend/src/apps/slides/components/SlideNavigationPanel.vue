ad03344e365de94befb1e766655b8d749defd16c
Gursheen Anand <gursheen@frappe.io>
2025-03-29 21:49:22 +0530

    fix: center align toolbar

--- frontend/src/apps/slides/components/SlideNavigationPanel.vue
+++ frontend/src/apps/slides/components/SlideNavigationPanel.vue
@@ -2,7 +2,7 @@
 	<!-- Slide Navigation Panel -->
 	<div
 		class="fixed z-20 h-[94.27%] w-48 border-r bg-white shadow-2xl shadow-gray-300 transition-all duration-300 ease-in-out"
-		:class="showNavigator ? 'left-0' : '-left-44'"
+		:class="showNavigator ? 'left-0' : '-left-48'"
 		@mouseenter="showCollapseShortcut = true"
 		@mouseleave="showCollapseShortcut = false"
 		@wheel="handleWheelEvent"
--- frontend/src/apps/slides/components/Toolbar.vue
+++ frontend/src/apps/slides/components/Toolbar.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="bg-white rounded-lg h-10 fixed bottom-10 right-[642px] shadow-2xl flex items-center gap-1 p-1"
+		class="bg-white w-56 rounded-lg h-10 fixed bottom-10 left-[50%-144px] shadow-2xl flex items-center gap-1 p-1 justify-center"
 	>
 		<Tooltip
 			v-for="option in contentActions"
