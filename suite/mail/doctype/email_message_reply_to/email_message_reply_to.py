2edda52c34728511b97f685638f5dd5bd9dc3672
s-aga-r <sagarsharma.s312@gmail.com>
2025-04-21 10:00:56 +0530

    refactor: make Email Message DocType non-virtual

--- suite/mail/doctype/email_message_reply_to/email_message_reply_to.py
+++ suite/mail/doctype/email_message_reply_to/email_message_reply_to.py
@@ -3,29 +3,9 @@
 
 # import frappe
 from frappe.model.document import Document
+from uuid_utils import uuid7
 
 
 class EmailMessageReplyTo(Document):
-	def db_insert(self, *args, **kwargs):
-		raise NotImplementedError
-
-	def load_from_db(self):
-		raise NotImplementedError
-
-	def db_update(self):
-		raise NotImplementedError
-
-	def delete(self):
-		raise NotImplementedError
-
-	@staticmethod
-	def get_list(filters=None, page_length=20, **kwargs):
-		pass
-
-	@staticmethod
-	def get_count(filters=None, **kwargs):
-		pass
-
-	@staticmethod
-	def get_stats(**kwargs):
-		pass
+	def autoname(self) -> None:
+		self.name = str(uuid7())
