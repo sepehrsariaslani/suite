d9f33ec10aa7e3d32bb94de9cc6943cae4c85eda
Gursheen Anand <gursheen@frappe.io>
2025-06-17 02:44:38 +0530

    fix: font color and weight hierarchy in properties

--- frontend/src/apps/slides/utils/constants.js
+++ frontend/src/apps/slides/utils/constants.js
@@ -1,5 +1,5 @@
 const sectionClasses = 'flex flex-col p-3 border-b'
-const sectionTitleClasses = 'text-base text-gray-700'
+const sectionTitleClasses = 'text-base font-medium text-gray-800'
 const fieldLabelClasses = 'text-sm text-gray-600'
 
 export { sectionClasses, sectionTitleClasses, fieldLabelClasses }
