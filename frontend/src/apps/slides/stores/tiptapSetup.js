6c733533359e04feac754c301ca704d65e432049
Gursheen Anand <gursheen@frappe.io>
2025-07-16 12:39:01 +0530

    refactor: simplify text style extensions

--- frontend/src/apps/slides/stores/tiptapSetup.js
+++ frontend/src/apps/slides/stores/tiptapSetup.js
@@ -1,14 +1,51 @@
 import { Editor } from '@tiptap/vue-3'
+import { Extension } from '@tiptap/core'
+
 import StarterKit from '@tiptap/starter-kit'
 import TextStyle from '@tiptap/extension-text-style'
 import TextAlign from '@tiptap/extension-text-align'
 import Underline from '@tiptap/extension-underline'
 import Color from '@tiptap/extension-color'
-
-import { Extension } from '@tiptap/core'
 import { Plugin } from 'prosemirror-state'
 
-export const PastePlainText = Extension.create({
+const CustomTextStyle = TextStyle.extend({
+	addAttributes() {
+		const attrs = {}
+
+		const attributes = [
+			'fontSize',
+			'textTransform',
+			'fontFamily',
+			'lineHeight',
+			'letterSpacing',
+			'opacity',
+		]
+
+		const suffixes = {
+			fontSize: 'px',
+			letterSpacing: 'px',
+			opacity: '%',
+		}
+
+		attributes.forEach((attr) => {
+			attrs[attr] = {
+				default: null,
+				parseHTML: (element) => element.style[attr] || null,
+				renderHTML: (attributes) => {
+					if (!attributes[attr] && attr != 'opacity') return {}
+					const attrName = attr.replace(/([A-Z])/g, '-$1').toLowerCase()
+					return {
+						style: `${attrName}: ${attributes[attr]}${suffixes[attr] || ''}`,
+					}
+				},
+			}
+		})
+
+		return attrs
+	},
+})
+
+const PastePlainText = Extension.create({
 	name: 'pastePlainText',
 
 	addProseMirrorPlugins() {
@@ -28,68 +65,6 @@ export const PastePlainText = Extension.create({
 		})
 
 		return [pastePlugin]
-	},
-})
-
-const CustomTextStyle = TextStyle.extend({
-	addAttributes() {
-		return {
-			fontSize: {
-				default: null,
-				parseHTML: (element) => element.style.fontSize || null,
-				renderHTML: (attributes) => {
-					if (!attributes.fontSize) return {}
-					return {
-						style: `font-size: ${attributes.fontSize}px`,
-					}
-				},
-			},
-			textTransform: {
-				default: null,
-				parseHTML: (el) => el.style.textTransform || null,
-				renderHTML: (attrs) =>
-					attrs.textTransform ? { style: `text-transform: ${attrs.textTransform}` } : {},
-			},
-			fontFamily: {
-				default: null,
-				parseHTML: (element) => element.style.fontFamily || null,
-				renderHTML: (attributes) => {
-					if (!attributes.fontFamily) return {}
-					return {
-						style: `font-family: ${attributes.fontFamily}`,
-					}
-				},
-			},
-			lineHeight: {
-				default: null,
-				parseHTML: (element) => element.style.lineHeight || null,
-				renderHTML: (attributes) => {
-					if (!attributes.lineHeight) return {}
-					return {
-						style: `line-height: ${attributes.lineHeight}`,
-					}
-				},
-			},
-			letterSpacing: {
-				default: null,
-				parseHTML: (element) => element.style.letterSpacing || null,
-				renderHTML: (attributes) => {
-					if (!attributes.letterSpacing) return {}
-					return {
-						style: `letter-spacing: ${attributes.letterSpacing}px`,
-					}
-				},
-			},
-			opacity: {
-				default: null,
-				parseHTML: (element) => element.style.opacity || null,
-				renderHTML: (attributes) => {
-					return {
-						style: `opacity: ${attributes.opacity}%`,
-					}
-				},
-			},
-		}
 	},
 })
 
