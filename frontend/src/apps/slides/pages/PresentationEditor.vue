25c901d8f229ebf21db674e4854e73526cee6132
Gursheen Anand <gursheen@frappe.io>
2025-08-06 16:37:17 +0530

    fix: keyboard shortcuts for position change

--- frontend/src/apps/slides/pages/PresentationEditor.vue
+++ frontend/src/apps/slides/pages/PresentationEditor.vue
@@ -117,6 +117,11 @@ const handleArrowKeys = (key) => {
 		left: selectionBounds.left + dx,
 		top: selectionBounds.top + dy,
 	})
+
+	activeElements.value.forEach((element) => {
+		element.left += dx
+		element.top += dy
+	})
 }
 
 const saveSlide = (e) => {
