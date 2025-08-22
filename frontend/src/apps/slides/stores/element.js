6c179646de36f9a017e75ccbd9c32fc326b1cd91
Gursheen Anand <gursheen@frappe.io>
2025-08-22 14:36:38 +0530

    fix: use png format for storing video posters

--- frontend/src/apps/slides/stores/element.js
+++ frontend/src/apps/slides/stores/element.js
@@ -148,7 +148,7 @@ const generatePoster = async (video) => {
 	const context = canvas.getContext('2d')
 	// draw the current frame of the video onto the canvas
 	context.drawImage(video, 0, 0, canvas.width, canvas.height)
-	const posterDataUrl = canvas.toDataURL('image/jpeg')
+	const posterDataUrl = canvas.toDataURL('image/png')
 
 	// save the poster as an attachment and return the url for the poster
 	return await savePoster.submit(posterDataUrl)
