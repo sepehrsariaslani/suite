eb46c97ddd4d9e4d431a5585406bbbb608757742
Gursheen Anand <gursheen@frappe.io>
2025-11-04 15:55:06 +0530

    feat: keep the text aligned to X axis center

--- frontend/src/apps/slides/components/SlideElement.vue
+++ frontend/src/apps/slides/components/SlideElement.vue
@@ -71,6 +71,8 @@ const elementStyle = computed(() => {
 		outline: props.highlight ? `#70B6F092 solid ${2 / slideBounds.scale}px` : 'none',
 		boxSizing: 'border-box',
 		zIndex: element.value.zIndex,
+		transform: `translate(-50%, 0%)`,
+		transformOrigin: 'center none',
 	}
 })
 
