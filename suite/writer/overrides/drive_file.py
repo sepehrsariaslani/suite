d6baae9fcd76244c7cd280be760b64d1918087de
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2025-12-01 18:11:01 +0530

    fix: stop using cached doc

--- suite/writer/overrides/drive_file.py
+++ suite/writer/overrides/drive_file.py
@@ -7,32 +7,28 @@ class WriterDriveFile(DriveFile):
     @frappe.whitelist(allow_guest=True)
     @requires("write")
     def add_yjs_update(self, update_b64):
-        return frappe.get_cached_doc("Writer Document", self.doc).add_yjs_update(
-            update_b64
-        )
+        return frappe.get_doc("Writer Document", self.doc).add_yjs_update(update_b64)
 
     @frappe.whitelist(allow_guest=True)
     @requires("write")
     def new_version(self, data, title=False):
-        return frappe.get_cached_doc("Writer Document", self.doc).new_version(
-            data, title
-        )
+        return frappe.get_doc("Writer Document", self.doc).new_version(data, title)
 
     @frappe.whitelist(allow_guest=True)
     def save_comments(self, data):
-        doc = frappe.get_cached_doc("Writer Document", self.doc)
+        doc = frappe.get_doc("Writer Document", self.doc)
         doc.ycomments = data
         doc.save()
 
     @frappe.whitelist(allow_guest=True)
     def update_settings(self, data):
-        doc = frappe.get_cached_doc("Writer Document", self.doc)
+        doc = frappe.get_doc("Writer Document", self.doc)
         doc.settings = data
         doc.save()
 
     @frappe.whitelist(allow_guest=True)
     def save_html(self, html):
-        doc = frappe.get_cached_doc("Writer Document", self.doc)
+        doc = frappe.get_doc("Writer Document", self.doc)
         doc.html = html
         doc.save()
         self._modified = frappe.utils.now()
