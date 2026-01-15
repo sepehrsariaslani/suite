8fc2ce47cef4ef25035f49654013aba51bbfb75d
Gursheen Anand <gursheen@frappe.io>
2026-01-15 13:20:16 +0530

    fix: cleanup sending response for non-cached requests

--- suite/slides/www/service-worker.js
+++ suite/slides/www/service-worker.js
@@ -57,43 +57,40 @@ const getModifiedResponse = (response) => {
 	})
 }
 
-self.addEventListener('fetch', (event) => {
+const getResponseForRequest = async (request) => {
+	// if the image is there in the cache, don't fetch from network
+	const cache = await caches.open(CACHE_NAME)
+	const cached = await cache.match(request, { ignoreSearch: false })
+	if (cached) return cached
+
+	// else fetch from network and cache it
+	const response = await fetch(request)
+
+	// check for valid response here - basically avoid caching if 404 or other error
+	if (response.ok && response.status === 200) {
+		const ct = response.headers.get('content-type') || ''
+		if (ct.startsWith('image/')) {
+			// clone response and add cache timestamp header
+			const modifiedResponse = getModifiedResponse(response)
+			cache.put(request, modifiedResponse)
+		}
+	}
+	return response
+}
+
+const handleSWFetch = async (event) => {
 	const request = event.request
 	if (request.method !== 'GET') return
 
 	// before fetching, check if it's a file request
 	const url = new URL(request.url)
-
 	if (url.origin !== self.location.origin) return
 	if (!url.pathname.startsWith('/private/files/')) return
 
-	event.respondWith(
-		(async () => {
-			// if the image is there in the cache, don't fetch from network
-			const cache = await caches.open(CACHE_NAME)
-			const cached = await cache.match(request, { ignoreSearch: false })
-			if (cached) return cached
+	const response = getResponseForRequest(request)
+	event.respondWith(response)
+}
 
-			// else fetch from network and cache it
-			try {
-				const response = await fetch(request)
-
-				// check for valid response here - basically avoid caching if 404 or other error
-				if (response.ok && response.status === 200) {
-					const ct = response.headers.get('content-type') || ''
-					if (ct.startsWith('image/')) {
-						// Clone response and add cache timestamp header
-						const modifiedResponse = getModifiedResponse(response)
-						cache.put(request, modifiedResponse)
-					}
-				}
-				return response
-			} catch (err) {
-				// if fetch fails, return cached version if available
-				// if there's no cached version, throw missing error
-				if (cached) return cached
-				throw err
-			}
-		})(),
-	)
+self.addEventListener('fetch', (event) => {
+	handleSWFetch(event)
 })
