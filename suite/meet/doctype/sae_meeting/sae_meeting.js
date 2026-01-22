7703c42d49152f997ab8fcb2c15cbc7a56910a55
Bread Genie <msuhailbh07@gmail.com>
2026-01-22 18:01:07 +0530

    feat: meet link from desk

--- suite/meet/doctype/sae_meeting/sae_meeting.js
+++ suite/meet/doctype/sae_meeting/sae_meeting.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2025, Frappe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Sae Meeting", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Sae Meeting", {
+	refresh(frm) {
+		frm.add_web_link(`/meet/${frm.doc.name}`, __("Join Meeting"));
+	},
+});
