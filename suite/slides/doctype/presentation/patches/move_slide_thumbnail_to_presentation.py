9f402887f601a1c2a44f453f8e508a6e900f9daf
Gursheen Anand <gursheen@frappe.io>
2026-05-24 14:29:49 +0530

    fix: use attach image field for new presentation thumbnail

--- suite/slides/doctype/presentation/patches/move_slide_thumbnail_to_presentation.py
+++ suite/slides/doctype/presentation/patches/move_slide_thumbnail_to_presentation.py
@@ -1,6 +1,10 @@
 import frappe
 
 
+def is_thumbnail_path(thumbnail):
+	return thumbnail and isinstance(thumbnail, str) and not thumbnail.startswith("data:")
+
+
 def execute():
 	if not frappe.db.has_column("Slide", "thumbnail"):
 		return
@@ -16,7 +20,7 @@ def execute():
 			{"parent": presentation.name, "idx": 1},
 			"thumbnail",
 		)
-		if first_slide_thumbnail:
+		if is_thumbnail_path(first_slide_thumbnail):
 			frappe.db.set_value(
 				"Presentation",
 				presentation.name,
