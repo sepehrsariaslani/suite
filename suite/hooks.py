2072bff789fcecc257faa7e36820164e8d24186c
netchamp.faris <netchamp.faris@gmail.com>
2026-06-15 18:13:53 +0530

    scaffold suite app skeleton

---
+++ suite/hooks.py
@@ -0,0 +1,7 @@
+app_name = "suite"
+app_title = "Frappe Suite"
+app_publisher = "Frappe"
+app_description = "Frappe Suite"
+app_license = "agpl-3.0"
+
+# ===== MERGED HOOKS APPENDED IN PHASE 3 =====
