frappe/mail

refactor: share Blocked/Junk Email Address per JMAP account ID
Drop the User and Account fields from Blocked Email Address and Junk
Email Address so both lists are shared across every user with JMAP
access to an account, keyed on the bare account_id — mirroring the
Account Settings refactor.

- Blocked/Junk are now scoped purely by account_id (Junk gains the
  field Blocked already had); duplicate checks, lookups, and the unique
  index move to (account_id, email).
- Add account-scoped permission helpers (get_account_scoped_permission_
  query / has_account_scoped_permission) in utils/user.py, reuse them in
  Account Settings, and register permission hooks for Blocked/Junk so
  non-admins only see the lists for accounts they belong to.
- API callers (block/junk/unblock/unjunk) set account_id; the acting
  user's handle is passed via flags so the sieve scripts can still be
  regenerated over JMAP.
- Add backfill_junk_email_account_id patch (mirrors the blocked one),
  guard both backfills against the dropped column, and drop Blocked
  from set_user_account.
- Simplify the desk JS (the user/account option pickers are gone).

Frontend needs no changes: it calls the APIs (account handle in, email
list out), whose contract is unchanged.

Co-Authored-By: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

mail/hooks.py
@@ -158,6 +158,8 @@ after_migrate = "mail.install.after_migrate"
 permission_query_conditions = {
 	# Client
 	"Account Settings": "mail.client.doctype.account_settings.account_settings.get_permission_query_condition",
+	"Blocked Email Address": "mail.client.doctype.blocked_email_address.blocked_email_address.get_permission_query_condition",
+	"Junk Email Address": "mail.client.doctype.junk_email_address.junk_email_address.get_permission_query_condition",
 	"Mail Exchange": "mail.client.doctype.mail_exchange.mail_exchange.get_permission_query_condition",
 	"Mail Queue": "mail.client.doctype.mail_queue.mail_queue.get_permission_query_condition",
 	"Mailbox Settings": "mail.client.doctype.mailbox_settings.mailbox_settings.get_permission_query_condition",
@@ -168,11 +170,13 @@ has_permission = {
 	# Client
 	"Account Settings": "mail.client.doctype.account_settings.account_settings.has_permission",
 	"Address Book": "mail.client.doctype.address_book.address_book.has_permission",
+	"Blocked Email Address": "mail.client.doctype.blocked_email_address.blocked_email_address.has_permission",
 	"Calendar": "mail.client.doctype.calendar.calendar.has_permission",
 	"Calendar Event": "mail.client.doctype.calendar_event.calendar_event.has_permission",
 	"Contact Card": "mail.client.doctype.contact_card.contact_card.has_permission",
 	"Event Notification": "mail.client.doctype.event_notification.event_notification.has_permission",
 	"Identity": "mail.client.doctype.identity.identity.has_permission",
+	"Junk Email Address": "mail.client.doctype.junk_email_address.junk_email_address.has_permission",
 	"Mail Exchange": "mail.client.doctype.mail_exchange.mail_exchange.has_permission",
 	"Mail Queue": "mail.client.doctype.mail_queue.mail_queue.has_permission",
 	"Mailbox": "mail.client.doctype.mailbox.mailbox.has_permission",

sepehrsariaslani/suite

suite/hooks.py
@@ -167,6 +167,8 @@ permission_query_conditions = {
 	"Sheet Snapshot": "suite.sheets.permissions.sheet_snapshot_query",
 	# mail (client)
 	"Account Settings": "suite.client.doctype.account_settings.account_settings.get_permission_query_condition",
+	"Blocked Email Address": "suite.client.doctype.blocked_email_address.blocked_email_address.get_permission_query_condition",
+	"Junk Email Address": "suite.client.doctype.junk_email_address.junk_email_address.get_permission_query_condition",
 	"Mail Exchange": "suite.client.doctype.mail_exchange.mail_exchange.get_permission_query_condition",
 	"Mail Queue": "suite.client.doctype.mail_queue.mail_queue.get_permission_query_condition",
 	"Mailbox Settings": "suite.client.doctype.mailbox_settings.mailbox_settings.get_permission_query_condition",
@@ -189,11 +191,13 @@ has_permission = {
 	# mail (client)
 	"Account Settings": "suite.client.doctype.account_settings.account_settings.has_permission",
 	"Address Book": "suite.client.doctype.address_book.address_book.has_permission",
+	"Blocked Email Address": "suite.client.doctype.blocked_email_address.blocked_email_address.has_permission",
 	"Calendar": "suite.client.doctype.calendar.calendar.has_permission",
 	"Calendar Event": "suite.client.doctype.calendar_event.calendar_event.has_permission",
 	"Contact Card": "suite.client.doctype.contact_card.contact_card.has_permission",
 	"Event Notification": "suite.client.doctype.event_notification.event_notification.has_permission",
 	"Identity": "suite.client.doctype.identity.identity.has_permission",
+	"Junk Email Address": "suite.client.doctype.junk_email_address.junk_email_address.has_permission",
 	"Mail Exchange": "suite.client.doctype.mail_exchange.mail_exchange.has_permission",
 	"Mail Queue": "suite.client.doctype.mail_queue.mail_queue.has_permission",
 	"Mailbox": "suite.client.doctype.mailbox.mailbox.has_permission",