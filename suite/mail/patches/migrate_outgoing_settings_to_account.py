frappe/mail

feat: move Outgoing settings from User Settings to Account Settings
Default outgoing email and the send-behaviour toggles (create contacts /
delete email / delete newsletter after sending) are now per-account, on
Account Settings (above the Cache section), instead of per-user on User
Settings.

- Schema: fields moved between doctypes; Account Settings readable/writable
  by the owning user.
- Backend: get_user_info attaches each account's default + settings doc;
  contacts/mail_queue read the toggles from Account Settings by account;
  the default-outgoing validation lives on Account Settings (falls back to
  the account's first identity).
- Frontend: the Account Settings tab binds the active account's settings
  (backup email stays per-user); compose picks the active account's default.
- Migration: capture old User Settings values pre-sync, copy verbatim post-
  sync, then a background job resolves each account's default against its
  identities (JMAP isn't reachable during migrate).

Co-Authored-By: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

mail/patches/migrate_outgoing_settings_to_account.py
@@ -1,0 +1,41 @@
+import frappe
+
+from mail.client.doctype.account_settings.account_settings import backfill_default_outgoing_emails
+from mail.patches.capture_user_outgoing_settings import CACHE_KEY
+
+
+def execute() -> None:
+	"""Apply the captured per-user Outgoing settings onto each Account Settings.
+
+	The three toggles and the old default_outgoing_email are copied verbatim to every
+	account the user owns (JMAP isn't reliably reachable during `bench migrate`, so we
+	can't resolve identities here). The per-account default is then finalised against
+	each account's identities by a background job once migrate is done.
+	"""
+
+	captured = frappe.cache.get_value(CACHE_KEY)
+	by_user = frappe.parse_json(captured) if captured else {}
+
+	for settings in frappe.get_all("Account Settings", fields=["name", "account", "user"]):
+		old = by_user.get(settings["user"])
+		if not old:
+			continue
+
+		frappe.db.set_value(
+			"Account Settings",
+			settings["name"],
+			{
+				"default_outgoing_email": old.get("default_outgoing_email"),
+				"create_contacts_after_email_submit": old.get("create_contacts_after_email_submit") or 0,
+				"destroy_email_after_submit": old.get("destroy_email_after_submit") or 0,
+				"destroy_newsletter_after_submit": old.get("destroy_newsletter_after_submit") or 0,
+			},
+			update_modified=False,
+		)
+
+	if captured:
+		frappe.cache.delete_value(CACHE_KEY)
+
+	# Resolve each account's default outgoing email against its identities once migrate
+	# is done — JMAP returns nothing while the migrate process is running.
+	frappe.enqueue(backfill_default_outgoing_emails, queue="long", enqueue_after_commit=True)

sepehrsariaslani/suite

suite/mail/patches/migrate_outgoing_settings_to_account.py
@@ -1,0 +1,41 @@
+import frappe
+
+from suite.client.doctype.account_settings.account_settings import backfill_default_outgoing_emails
+from suite.mail.patches.capture_user_outgoing_settings import CACHE_KEY
+
+
+def execute() -> None:
+	"""Apply the captured per-user Outgoing settings onto each Account Settings.
+
+	The three toggles and the old default_outgoing_email are copied verbatim to every
+	account the user owns (JMAP isn't reliably reachable during `bench migrate`, so we
+	can't resolve identities here). The per-account default is then finalised against
+	each account's identities by a background job once migrate is done.
+	"""
+
+	captured = frappe.cache.get_value(CACHE_KEY)
+	by_user = frappe.parse_json(captured) if captured else {}
+
+	for settings in frappe.get_all("Account Settings", fields=["name", "account", "user"]):
+		old = by_user.get(settings["user"])
+		if not old:
+			continue
+
+		frappe.db.set_value(
+			"Account Settings",
+			settings["name"],
+			{
+				"default_outgoing_email": old.get("default_outgoing_email"),
+				"create_contacts_after_email_submit": old.get("create_contacts_after_email_submit") or 0,
+				"destroy_email_after_submit": old.get("destroy_email_after_submit") or 0,
+				"destroy_newsletter_after_submit": old.get("destroy_newsletter_after_submit") or 0,
+			},
+			update_modified=False,
+		)
+
+	if captured:
+		frappe.cache.delete_value(CACHE_KEY)
+
+	# Resolve each account's default outgoing email against its identities once migrate
+	# is done — JMAP returns nothing while the migrate process is running.
+	frappe.enqueue(backfill_default_outgoing_emails, queue="long", enqueue_after_commit=True)